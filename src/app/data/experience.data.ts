6357f7563b2335131b2a682b6053240b41bc2b39
LaraYuka <larayuka.sak@gmail.com>
2026-01-21 11:24:00 -0300

    alterando experiencias

--- src/app/data/experience.data.ts
+++ src/app/data/experience.data.ts
@@ -2,10 +2,26 @@ import { Experience } from '../models/experience.model';
 
 export const EXPERIENCES: Experience[] = [
   {
-    role: 'Monitora de Creche (Concursada)',
-    company: 'CMEI Sonho Meu',
-    location: 'Mariópolis - PR',
-    period: '2024 - 2025',
-    description: 'Atuação no cuidado e acompanhamento de crianças, auxiliando no desenvolvimento pedagógico, ajudando na organização de atividades lúdicas, apoio às professoras e zelo pelo bem-estar e segurança dos alunos.'
-  }
+  role: 'Monitora de Creche (Concursada)',
+  company: 'CMEI Sonho Meu',
+  location: 'Mariópolis - PR',
+  period: '2024 - 2025',
+  description: 'Atuação no acompanhamento de crianças e apoio às rotinas pedagógicas, desenvolvendo forte senso de responsabilidade, organização e comunicação. Experiência em trabalho colaborativo, resolução de problemas e cumprimento de prazos.'
+},
+{
+  role: 'Estagiária em Análise e Desenvolvimento de Sistemas',
+  company: 'CMEI Sonho Meu',
+  location: 'Mariópolis - PR',
+  period: '2024',
+  description: 'Atuação prática na análise da rotina institucional e levantamento de requisitos funcionais e não funcionais para um sistema de gestão de materiais pedagógicos. Elaboração de diagramas, protótipos e apoio ao desenvolvimento de uma solução web voltada à organização, controle e sustentabilidade no uso de recursos educacionais.'
+},
+{
+  role: 'Projeto de TCC – Sistema GEMAP',
+  company: 'Faculdade de Tecnologia em Análise e Desenvolvimento de Sistemas',
+  location: 'Mariópolis - PR',
+  period: '2025',
+  description: 'Desenvolvimento de um sistema web para gestão de materiais pedagógicos, envolvendo análise de requisitos, modelagem de dados, definição de arquitetura e implementação. O projeto contempla funcionalidades de cadastro, agendamento, relatórios e exportação de dados, com foco em eficiência administrativa e apoio à prática pedagógica.'
+}
+
+
 ];
